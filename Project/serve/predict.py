e24c5d31591ba945cc4a36dd5e4308b05edeaf5f
Moulika <smoulikas3@gmail.com>
2020-09-12 10:48:09 +0000

    added code for training the dataset

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -86,7 +86,10 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
+    with torch.no_grad():
+        output = model.forward(data)
 
-    result = None
+    result = np.round(output.numpy())
+    print(result)
 
     return result
